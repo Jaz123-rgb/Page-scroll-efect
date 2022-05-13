26b606524893a3483fe87aeda3d05bb021151bc8
Jaziel Perez <pjaziel723@gmail.com>
2022-05-13 09:35:54 -0500

    pagina con parallax efect

---
+++ anim.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let text = document.getElementById('text');
+let bird1 = document.getElementById('bird1');
+let bird2 = document.getElementById('bird2');
+let btn = document.getElementById('btn');
+let forest = document.getElementById('forest');
+let rocks = document.getElementById('rocks'); 
+let water = document.getElementById('water');
+let header = document.getElementById('header');
+
+window.addEventListener('scroll',()=>{
+   let value = window.scrollY;
+   text.style.top = 20 + value * -0.5 + '%';
+   bird1.style.top = value * -1.5 + 'px';
+   bird1.style.left = value * 2 + 'px';
+   bird2.style.top = value * -1.5 + 'px';
+   bird2.style.left = value * -5 + 'px';
+   btn.style.marginTop = value * 1.5 + 'px';
+   rocks.style.top = value * -0.12 + 'px'; 
+   forest.style.top = value * 0.25 + 'px';
+   header.style.top = value * 0.5 + 'px';
+});
